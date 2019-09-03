47b0ef60a7547d8f753b14e6566adf86eae8e58c
gellel <lindsay.gelle@gmail.com>
2019-09-03 20:48:43 +1000

    Segmenting program into files

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"net/http"
@@ -35,7 +36,7 @@ func (gameCatalogue gameCatalogue) Has(key string) bool {
 	_, ok := gameCatalogue.Get(key)
 	return ok
 }
-	
+
 type queryMap map[string]string
 
 func (queryMap queryMap) Add(key, value string) bool {
@@ -124,49 +125,6 @@ func (queryCategories queryCategories) Set(tag string) bool {
 	return (ok == false)
 }
 
-// Game is a struct that expresses a video game that has been scraped from the Steam store. It
-// contains properties and attributes that aim to provide a snapshot of the game's current
-// state on the date it was collected. A Game is expected to be built by querying and
-// reducing HTML elements found across the Steam game page. As this data is generally
-// provided from a raw HTML document, and each field may have varying levels of artifacting.
-// Most artifacting has been handled by various string mutation operations but inconsistencies
-// may persist.
-type game struct {
-	AppID              string            `json:"appid"`              // {AppID: "306130"} OR {AppID: "NIL"} (NIL when BundleID)
-	BundleID           string            `json:"bundleid"`           // {BundleID: "NIL"} OR {BundleID: "11802"} (NIL when AppID)
-	Categories         []gameCategory    `json:"categories"`         // {Categories: [MMO Steam Trading Cards Partial Controller Support]}
-	CrtrID             []string          `json:"crtrid"`             // {CrtrID: "[33028765,35501445]"}
-	DescriptionID      []string          `json:"descriptionid"`      // {DescriptionID: "[2,5]"}
-	Description        string            `json:"description"`        // {Description: "...."}
-	DescriptionVerbose string            `json:"descriptionverbose"` // {DescriptionVerbose: "...."}
-	Developer          []gameDeveloper   `json:"developer"`          // {Developer: [Zenimax Online Studios]}
-	Genre              []gameGenre       `json:"genre"`              // {Genre: [Massively Multiplayer RPG]}
-	Languages          []gameLanguage    `json:"languages"`          // {Languages: [{English Audio true Interface true Subtitles true}]}
-	Meta               []gameMeta        `json:"meta"`               // {Meta: [...]}
-	Name               string            `json:"name"`               // {Name: "THE-ELDER-SCROLLS-ONLINE"}
-	PackageID          string            `json:"packageid"`          // {PackageID: "124923"}
-	Publisher          []gamePublisher   `json:"publisher"`          // {Publisher: [ZENIMAX-ONLINE-STUDIOS ...]}
-	ReleaseDate        string            `json:"releasedate"`        // {ReleaseDate: "4 Apr, 2014"}
-	Requirements       []gameRequirement `json:"requirements"`       // {Requirements: {DirectX: Version 11 Graphics: AMD Radeon RX 470 Memory: ...}]
-	TagID              []string          `json:"tagid"`              // {TagID: "[19,1685,3814,29482,122,3859,21]"}
-	Tags               []gameTag         `json:"tags"`               // {Tags: [Post-apocalyptic Difficult Survival Lovecraft ...]}
-	Title              string            `json:"title"`              // {Title: "The Elder Scrolls® Online"}
-	URL                string            `json:"url"`                // {URL: "https://store.steampowered.com/app/306130/The_Elder_Scrolls_Online/?snr=1_7_7_230_150_1"}
-}
-
-
-// GameCategory is a struct that expresses the individual attribute of the Steam game. Unlike a GameGenre, a GameCategory
-// describes the unique attributes at a feature level that help distinguish the options one game offers over another.
-type gameCategory struct {
-	Name  string `json:"name"`  // {Name: "SINGLE-PLAYER"}
-	Title string `json:"title"` // {Title: "Single Player"}
-	URL   string `json:"url"`   // {URL: "https://store.steampowered.com/search/?category2=2"}
-}
-
-func (gameCategory gameCategory) String() string {
-	return fmt.Sprintf("%s", gameCategory.Name)
-}
-
 // GameDeveloper is a struct that expresses the individual or group that was responsible for creating the Steam game.
 type gameDeveloper struct {
 	Name  string `json:"name"`  // {Name: "ROCKSTAR-NORTH"}
@@ -215,10 +173,6 @@ type gamePublisher struct {
 	Name  string `json:"name"`  // {Name: "ROCKSTAR-GAMES"}
 	Title string `json:"title"` // {Title: "Rockstar Games"}
 	URL   string `json:"url"`   // {URL: "https://store.steampowered.com/publisher/rockstargames"}
-}
-
-func (gamePublisher gamePublisher) String() string {
-	return fmt.Sprintf("%s", gamePublisher.Name)
 }
 
 func (gamePublisher gamePublisher) String() string {
@@ -661,7 +615,7 @@ func getUserPageOffset() (int, error) {
 	var n int
 	fmt.Println("Steamer.exe\t| PLEASE ENTER PAGE NUMBER TO BEGIN SCRAPING FROM:")
 	if ok := scanner.Scan(); ok != true {
-		return n, errors.New("user did not provide input")
+		return n, errors.New("input err")
 	}
 	n, err := strconv.Atoi(scanner.Text())
 	if err != nil {
@@ -735,4 +689,3 @@ func main() {
 		panic(err)
 	}
 }
-
